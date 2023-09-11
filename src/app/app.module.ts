3eea6717394c41c0e6c2d4f0eca27bc53af26b33
Asmaa Hassan Hamza <Eng.asmaahassanhamza98@gmail.com>
2023-09-11 14:31:39 +0300

    add tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,32 @@ import { Test2Component } from './components/test2/test2.component';
 import { Test3Component } from './components/test3/test3.component';
 import { ReactiveFormsModule ,FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
     Test2Component,
-    Test3Component
+    Test3Component,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule,
+    MatSlideToggleModule
+    
     
   ],
   providers: [],
